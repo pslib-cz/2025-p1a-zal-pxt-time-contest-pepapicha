583efe035c61294cb4289918437bacf485daac1f
Pepa Pícha <107387653+pepapicha@users.noreply.github.com>
2026-04-13 10:38:56 +0200

    final

--- main.ts
+++ main.ts
@@ -10,3 +10,36 @@ let state: GameState = GameState.Passive
 let targetInterval: number = 0   // sekundy (5–15)
 let startTime: number = 0        // ms – základ pro měření
 let score: number = 0
+
+input.onButtonPressed(Button.AB, function () {
+    if (state === GameState.Passive) {
+        state = GameState.Started
+        targetInterval = randint(5, 15)
+        score = 0
+        basic.showIcon(IconNames.Pitchfork)
+        control.runInBackground(() => music.playTone(440, 200))
+        basic.pause(targetInterval * 1000)
+        control.runInBackground(() => music.playTone(880, 200))
+        state = GameState.Running
+        basic.showString("?")
+        startTime = control.millis()
+    }
+})
+
+input.onLogoEvent(TouchButtonEvent.Touched, function () { 
+    if (state === GameState.Running){
+        let elapsed = control.millis() - startTime //čas, který uběhl od konce přehrávání do stisku hráče
+        let targetMs = targetInterval * 1000 //převod cílového intervalu na milisekundy
+        let tolerance = 250 + (0.1 * targetMs) //odchylka: 250 ms + 10% z celkového času
+        
+        if (elapsed >= (targetMs - tolerance) && elapsed <= targetMs) {
+            basic.showIcon(IconNames.Happy)
+            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground);
+        } else {
+            basic.showIcon(IconNames.Sad)
+            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Wawawawaa), music.PlaybackMode.InBackground);
+        }
+    }
+})
+
+state = GameState.Passive
